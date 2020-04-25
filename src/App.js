5465500ca9e99188a31d6e3bb7e6f1d3808f3f99
slayeroak <roakley1@villanova.edu>
2020-04-24 23:10:16 -0400

    working components, added char images, images.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import images from "./images.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    images,
+    score: 0,
+    topscore: 0,
+    clickedarray: []
+  };
+
+  randomOrder = () => {
+    return this.state.images.sort(() => Math.random() - 0.5);
+  };
+
+  handleImageClick = event => {
+    const currentLegend = event.target.id;
+
+    const legendClicked = this.state.clickedarray.indexOf(currentLegend) > -1;
+
+    if (legendClicked) {
+      this.setState({
+        images: this.randomOrder(),
+        clickedarray: [],
+        score: 0
+      });
+    } else {
+      this.setState({
+        images: this.randomOrder(),
+        clickedarray: this.state.clickedarray.concat(currentLegend),
+        score: this.state.score + 1
+      });
+
+      //update topscore
+      if (this.state.score >= this.state.topscore) {
+        this.setState({ topscore: this.state.score + 1 });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar score={this.state.score} topscore={this.state.topscore} />
+        <Header />
+        <Main handleimageclick={this.handleImageClick} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(props) {
+    return (
+        <div className="navbar sticky-bottom">
+            <h3 className="navbar-brand">Ryan Oakley</h3>
+            <h3 className="navbar-brand">Slayeroak Legends</h3>
+        </div>
+    );
+}
+
+export default Footer;

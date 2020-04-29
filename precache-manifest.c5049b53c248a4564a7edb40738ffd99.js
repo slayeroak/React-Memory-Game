f2ea10cc518932b29ef7d33f7dea961ea8817c44
slayeroak <roakley1@villanova.edu>
2020-04-28 22:28:56 -0400

    Updates

---
+++ precache-manifest.c5049b53c248a4564a7edb40738ffd99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da942e0502d002bd9166c00a655ebc12",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "5872bca4cdfeb088d68c",
+    "url": "/React-Memory-Game/static/css/main.d4a4f9af.chunk.css"
+  },
+  {
+    "revision": "c8a399535194478d0988",
+    "url": "/React-Memory-Game/static/js/2.02a007a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Memory-Game/static/js/2.02a007a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5872bca4cdfeb088d68c",
+    "url": "/React-Memory-Game/static/js/main.3eab7551.chunk.js"
+  },
+  {
+    "revision": "2191656ecf9e55de852f",
+    "url": "/React-Memory-Game/static/js/runtime-main.b492e65e.js"
+  }
+]);
